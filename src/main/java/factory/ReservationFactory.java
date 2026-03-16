a7ef3b85faaaa3b110599b4f8b3a25828db4de82
Abulele75 <ntwanambiabulele@gmail.com>
2026-03-16 19:32:44 +0200

    Add Reservation class and ReservationFactory

--- src/main/java/factory/ReservationFactory.java
+++ src/main/java/factory/ReservationFactory.java
@@ -10,5 +10,18 @@ import java.time.LocalDate;
 
 public class ReservationFactory
 {
+    public static Reservation createReservation(String reservationID, LocalDate date, String status) {
 
+        if (reservationID == null || date == null || status == null) {
+            return null;
+        }
+        return new Reservation.Builder()
+                .setReservationID(reservationID)
+                .setDate(date)
+                .setStatus(status)
+                .build();
+
+    }
 }
+
+
