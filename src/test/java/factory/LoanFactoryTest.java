1c1e7f14b7846a2cabc57da2865db53e586a988e
Sinazo35 <sinazo.ntsimbi@gmail.com>
2026-03-22 20:03:51 +0200

    Updated Loan&LoanFactory.Added LoanFactoryTest

--- src/test/java/factory/LoanFactoryTest.java
+++ src/test/java/factory/LoanFactoryTest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
  class LoanFactoryTest {
-
 	@Test
 	void shouldCreateLoanUsingBuilder() {
 
