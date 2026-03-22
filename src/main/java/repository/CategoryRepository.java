87f87910255015b479963e56a994815daffe70d1
Tiyani Ngwana <ngwanatiyanitn@gmail.com>
2026-03-22 07:12:48 +0200

    Update author information in CategoryRepository
    Signed-off-by: Tiyani Ngwana <ngwanatiyanitn@gmail.com>

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
 /* CategoryRepository.java
    Category repository interface
-   Author: Abulele Ntwanambi (218276400)
+   Author: Tiyani Ngwana 231266731
    Date: 13 March 2026
 */
 package repository;
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends IRepository<Category, String> {
-    // Additional query methods specific to Category
+    
     Optional<Category> findByName(String name);
     List<Category> findByNameContaining(String keyword);
     boolean existsByName(String name);
